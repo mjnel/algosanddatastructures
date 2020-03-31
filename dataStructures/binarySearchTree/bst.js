193fd8967d054b3c073d381328d6fdc0bd9eed18
Mark Nelson <mark.nelson@synchronoss.com>
2020-03-31 10:54:00 +0100

    working less than work on bst

--- dataStructures/binarySearchTree/bst.js
+++ dataStructures/binarySearchTree/bst.js
@@ -20,14 +20,90 @@ class BinarySearchTree{
         if(!this.root){
             this.root = newNode
             return this;
+        }else{
+
+        let current = this.root
+       
+        while (true){
+            // duplicate checker
+            if(val === current.val) return undefined
+
+            if(val < current.value){
+                if(current.left === null){
+                    current.left = newNode
+                    return this
+                }else{
+                    current = current.left  
+                }
+
+            }else if(val > current.value){
+                if(current.right === null){
+                    current.right = newNode
+                    return this
+                }else{
+                    current = current.right 
+                }
+
+                }
+
+            }
+
+
         }
 
 
     }
-}
 
-let test = new BinarySearchTree
 
+    find(val){
+        console.log(`finding ${val}`)
+        if(!this.root){
+            return null
+        }  
+    
+    let current = this.root
+
+    while(true){
+        if(val === current.value){
+            // debugger
+            return true
+        }else if(val < current.value){
+            // debugger
+            current = current.left
+        }else{
+            // debugger
+            return null
+            }
+ 
+        }
+    }
+    
+
+
+
+
+
+
+
+   
+
+     
+    
+
+  
+    }
+    
+
+let tree = new BinarySearchTree
+
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(2)
+tree.insert(16)
+tree.insert(7)
+let test = tree.find(2)
 debugger;
 
 
