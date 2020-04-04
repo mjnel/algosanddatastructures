114972037c8e0abd496338b1d4032d5ed31d0e1a
Mark Nelson <mark.nelson@synchronoss.com>
2020-04-04 20:32:39 +0100

    my solution for heap remeoval

---
+++ dataStructures/binaryHeap/maxBinaryHeap.js
@@ -0,0 +1,117 @@
+
+
+class MaxBinaryHeap{
+    constructor(){
+        // this.valuesList = [55,39,41,18,27,12,33]
+        this.valuesList = [41,39,33,18,27,12]
+    }
+
+
+
+
+    insert(element){
+        this.valuesList.push(element)
+        this.bubbleUp()
+        return this.valuesList
+
+    }
+
+
+    bubbleUp(){
+        let idx = this.valuesList.length -1
+        const element = this.valuesList[idx]
+        // debugger;
+        while(idx > 0){
+            let parentIndex = Math.floor((idx -1)/2)
+            let parent = this.valuesList[parentIndex]
+            // debugger
+            if(element<= parent){
+                break
+            }
+            this.valuesList[parentIndex] = element;
+            this.valuesList[idx]= parent
+            idx = parentIndex
+            
+        }
+
+    }
+
+    // removing the max item
+    // remove(){
+    //     // debugger
+ 
+
+    //     let endValue = this.valuesList[this.valuesList.length-1]
+    //     let startValue = this.valuesList[0]
+    //     this.valuesList.shift()
+    //     this.valuesList.unshift(endValue)
+    //     this.valuesList.pop()
+    //     this.sinkDown(0)
+    //     return startValue
+   
+    // }
+
+
+    // sinkDown(nodeLocation){
+    //     let tempParent = this.valuesList[nodeLocation] 
+    //     let leftValue = this.valuesList[(2*nodeLocation)+1] 
+    //     let rightValue = this.valuesList[(2*nodeLocation)+2] 
+    //     let leftSideBigger = false
+    //     // debugger;
+        
+    //     //break case - as its a recurisve function
+
+    //     if(leftValue === undefined || rightValue === undefined){
+    //         // debugger
+    //         return this.valuesList
+    //     }
+
+    //     // if the node is above both the left and right then return sinkDown function
+    //     if(tempParent > leftValue && tempParent > rightValue){
+    //         debugger;
+    //         return this.valuesList
+    //     }else{
+    
+    //     if(leftValue > rightValue){
+    //             leftSideBigger = true
+    //         }   
+    //     // if the left is bigger
+    //     if(leftSideBigger){       
+    //                 let leftIndex = (2*nodeLocation)+1
+    //                 let temp = this.valuesList[nodeLocation]
+
+    //                 this.valuesList[nodeLocation]= leftValue
+    //                 this.valuesList[leftIndex] = temp
+    //                 // debugger
+    //                 this.sinkDown(leftIndex)
+
+    //         }else{
+    //                 let rightIndex = (2*nodeLocation)+2
+    //                 let temp = this.valuesList[nodeLocation]
+
+    //                 this.valuesList[nodeLocation]= rightValue
+    //                 this.valuesList[rightIndex] = temp
+    //                 // debugger; 
+    //                 this.sinkDown(rightIndex)
+    //         }
+    //     }   
+
+    //     // debugger
+
+
+    // }
+
+}
+
+// [41,39,33,18,27,12]
+
+
+let heap = new MaxBinaryHeap();
+// heap.insert(55)
+// heap.insert(12)
+// heap.insert(77)
+// heap.insert(23)
+// heap.insert(19)
+// heap.insert(1)
+// heap.insert(3)
+debugger;
--- dataStructures/binarySearchTree/bst.js
+++ dataStructures/binarySearchTree/bst.js
@@ -58,7 +58,7 @@ class BinarySearchTree{
     find(val){
         console.log(`finding ${val}`)
         if(!this.root){
-            return null
+            return false
         }  
     
     let current = this.root
@@ -66,20 +66,177 @@ class BinarySearchTree{
     while(true){
         if(val === current.value){
             // debugger
-            return true
-        }else if(val < current.value){
+            return current
+        }else if(val < current.value && current.left){
             // debugger
             current = current.left
-        }else{
+        }
+        else if(val > current.value && current.right){
+            // debugger;
+            current = current.right
+        }
+        else{
             // debugger
-            return null
+            return false
             }
  
         }
     }
+
+// colts solution
+
+    // find(value){
+    //     if(this.root === null) return false;
+    //     var current = this.root,
+    //         found = false;
+    //     while(current && !found){
+    //         if(value < current.value){
+    //             current = current.left;
+    //         } else if(value > current.value){
+    //             current = current.right;
+    //         } else {
+    //             found = true;
+    //         }
+    //     }
+    //     if(!found) return undefined;
+    //     return current;
+    // }
+    // contains(value){
+    //     if(this.root === null) return false;
+    //     var current = this.root,
+    //         found = false;
+    //     while(current && !found){
+    //         if(value < current.value){
+    //             current = current.left;
+    //         } else if(value > current.value){
+    //             current = current.right;
+    //         } else {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
     
 
+breathFirstSearch(){
+        let q = []
+        let result = []
 
+        // push and shift
+        q.push(this.root)
+        // debugger
+
+        while(q.length != 0){
+            // debugger
+            let current = q.shift();
+
+            if(current.left){
+                q.push(current.left)
+            }
+
+            if(current.right){
+                q.push(current.right)
+            }
+            result.push(current)
+            // debugger;
+    }
+    return result
+}
+
+
+
+// explore node an then put in return array
+
+depthFirstSearchPreOrder(){
+
+    let nodesVisitied = []
+    let current = this.root
+    
+    // nodesVisitied.push(current)
+
+    let traverse = (node)=>{
+        nodesVisitied.push(node.value)
+        // debugger;
+        if(node.left){
+            // debugger
+            traverse(node.left)
+        }
+        if(node.right){
+            traverse(node.right)
+
+        }
+    }
+
+    traverse(current);
+    // debugger
+    return nodesVisitied
+
+
+}
+
+// explore all childern and then put in retuen array
+
+depthFirstSearchPostOrder(){
+
+    let nodesVisitied = []
+    let current = this.root
+    
+    // nodesVisitied.push(current)
+
+    let traverse = (node)=>{
+        // nodesVisitied.push(node.value)
+        // debugger;
+        if(node.left){
+            // debugger
+            traverse(node.left)
+        }
+        if(node.right){
+            traverse(node.right)
+
+        }
+        nodesVisitied.push(node.value)
+
+    }
+
+    traverse(current);
+    // debugger
+    return nodesVisitied
+
+}
+
+
+
+depthFirstSearchInOrder(){
+    let nodesVisitied = []
+    let current = this.root
+    
+    // nodesVisitied.push(current)
+
+    let traverse = (node)=>{
+        // nodesVisitied.push(node.value)
+        // debugger;
+        if(node.left){
+            // debugger
+            traverse(node.left)
+        }       
+        
+        nodesVisitied.push(node.value)
+
+        if(node.right){
+            traverse(node.right)
+
+        }
+
+    }
+
+    traverse(current)
+
+
+    debugger
+    return nodesVisitied
+}
+
+ 
 
 
 
@@ -103,8 +260,12 @@ tree.insert(11)
 tree.insert(2)
 tree.insert(16)
 tree.insert(7)
-let test = tree.find(2)
-debugger;
+tree.depthFirstSearchPostOrder()
+tree.breathFirstSearch()
+tree.depthFirstSearchInOrder()
+
+
+// debugger
 
 
 
